e3f672647f39a15369f47578105f82724824be85
Michal Dvořák <Michal.Dvorak@ncrvoyix.com>
2024-07-04 12:15:35 +0200

    Add lint rules, configs and ignores.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,47 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ["*.ts", "*.js"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@angular-eslint/recommended",
+      "plugin:@angular-eslint/template/process-inline-templates",
+      "prettier"
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          "type": "attribute",
+          "prefix": "app",
+          "style": "camelCase"
+        }
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          "type": "element",
+          "prefix": "app",
+          "style": "kebab-case"
+        }
+      ]
+    }
+  },
+  {
+    files: ["*.html"],
+    extends: [
+      "plugin:@angular-eslint/template/recommended",
+      "plugin:@angular-eslint/template/accessibility",
+      "prettier"
+    ],
+    "rules": {
+      "@angular-eslint/template/click-events-have-key-events": "off",
+      "@angular-eslint/template/interactive-supports-focus": "off",
+      "@angular-eslint/template/label-has-associated-control": "off",
+      "@angular-eslint/template/alt-text": "off"
+    }
+  }
 ];
